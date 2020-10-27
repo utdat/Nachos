1e9dd7b617b33a6a7b23d33cc56eeb1cabd08d26
bird <bird@localhost.localdomain>
2020-10-27 08:20:49 +0700

    Implement syscall Read

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -246,13 +246,80 @@ HandleSyscallOpen()
 
 
 // Handle syscall Read
-// TODO: Describe this function
+// Read data from file with file id given
+// If file is stdin and stdout, use console IO instead of file system
+// Return number of bytes read/written
+// Return -1 on error, -2 on eof reached
 void
 HandleSyscallRead()
 {
-	DEBUG('a', "\nUnexpected exception Syscall Read: Not impelemted");
-	printf("\nUnexpected exception Syscall Write: Not impelemted");
-	interrupt->Halt();
+	// Fetch param
+	int bufferAddr = machine->ReadRegister(4);
+	int byteCount = machine->ReadRegister(5);
+	int fileId = machine->ReadRegister(6);
+	
+	// Get file from file system
+	OpenFile* file = fileSystem->FileAt(fileId);
+
+	// Check for file with given id not found
+	if (file == NULL)
+	{
+		DEBUG('a', "\nUnexpected error: File system could not provide file with given id");
+		printf("\nUnexpected error: File system could not provide file with given id");
+		machine->WriteRegister(2, -1);
+		return;
+	}
+
+	// Check for reading stdout
+	if (file->Type() == 3)
+	{
+		DEBUG('a', "\nUnexpected error: Could not read from stdout");
+		printf("\nUnexpected error: Could not read from stdout");
+		machine->WriteRegister(2, -1);
+		return;
+	}
+
+	// Allocate space for data read. 1 more byte for null terminated charecter
+	char* buffer = new char[byteCount];
+
+	// Read data
+	int byteRead;
+	if (file->Type() == 2)
+	{	
+		// Read from console. Reserved final byte from null character
+		byteRead = gSynchConsole->Read(buffer, byteCount - 1);
+		if (byteRead > 0)
+		{
+			// Read successfully
+			buffer[byteRead] = '\0';
+			System2User(bufferAddr, byteRead + 1, buffer);
+			machine->WriteRegister(2, byteRead);
+		}
+		else
+		{
+			// End of file
+			machine->WriteRegister(2, -2);
+		}
+	}
+	else
+	{
+		// Read from file
+		byteRead = file->Read(buffer, byteCount);
+		if (byteRead > 0)
+		{
+			// Read successfully
+			System2User(bufferAddr, byteRead, buffer);
+			machine->WriteRegister(2, byteRead);
+		}
+		else
+		{
+			// End of file
+			machine->WriteRegister(2, -2);
+		}
+	}
+	
+	// Free buffer space
+	delete[] buffer;
 }
 
 
