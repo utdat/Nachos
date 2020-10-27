26ed0d33a75d8e1fa9275f2523781049f74b17bc
ThaiDat <tandatcr2000pro@yahoo.com>
2020-10-27 23:21:17 +0700

    Add scheduler program to test Exec
    Test failed

---
+++ test/scheduler.c
@@ -0,0 +1,19 @@
+/* scheduler.c
+ * Simple program that simultaneusly run ping and pong 
+ * (print A/B 1000 times each simultaneusly)
+*/
+
+#include "syscall.h"
+#define SCHEDULER_INS "***Scheduler program | Simultaneusly Ping and Pong***\n" 
+#define SCHEDULER_INS_LEN 54
+#define PING "./test/ping"
+#define PONG "./test/pong"
+
+int main()
+{
+	int pingId, pongId, i;
+	PrintS(SCHEDULER_INS, SCHEDULER_INS_LEN);
+	pingId = Exec(PING);
+	pongId = Exec(PONG);
+	return 0;
+}
